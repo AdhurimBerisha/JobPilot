080d061fb997217f2d9dd6defb53583726748c4d
AdhurimBerisha <adhurimberisha07@gmail.com>
2026-02-17 13:53:56 +0100

    Image Tabs

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import ImageTabs from "@/components/image-tabs";
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
@@ -27,6 +28,9 @@ export default function Home() {
             </div>
           </div>
         </section>
+
+        {/* Image Tabs */}
+        <ImageTabs />
       </main>
     </div>
   );
