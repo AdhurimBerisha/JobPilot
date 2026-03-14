324974a069969cee1224cb9bc9b2c2579ff80312
AdhurimBerisha <adhurimberisha07@gmail.com>
2026-03-14 01:23:58 +0100

    JobApplicationCard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,7 +6,7 @@ import KanbanBoard from "@/components/kaban-board";
 import { Suspense } from "react";
 
 async function getBoard(userId: string) {
-  // "use cache";
+  "use cache";
 
   await connectDB();
 
